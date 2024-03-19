aa9cca53d7e67972ad08fc0ec8f1af064907d61e
SidhantSwaraj <150464583+SidhantSwaraj@users.noreply.github.com>
2024-03-19 20:30:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 
 export default function App() {
   const [countries, setCountries] = useState([]);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
@@ -11,18 +12,33 @@ export default function App() {
       .catch((error) => console.error("Error fetching data: ", error));
   }, []);
 
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className="container">
-      {countries.map((country) => (
-        <div className="cardstyle" key={country.cca3}>
-          <img
-            className="imagestyle"
-            src={country.flags.png}
-            alt={`Flag of ${country.name.common}`}
-          />
-          <h2>{country.name.common}</h2>
-        </div>
-      ))}
+    <div>
+      <div className="search">
+        <input
+          type="text"
+          placeholder="Search for countries"
+          style={{ width: "70%" }}
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+      <div className="container">
+        {filteredCountries.map((country) => (
+          <div className="countryCard" key={country.cca3}>
+            <img
+              className="imagestyle"
+              src={country.flags.png}
+              alt={`Flag of ${country.name.common}`}
+            />
+            <h2>{country.name.common}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
